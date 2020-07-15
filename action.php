7af397152f92dd817e22c175d50910e2eb5a5d32
Aniket257 <ani.khajanchi257@gmail.com>
2020-07-15 13:26:27 +0530

    first commit

---
+++ action.php
@@ -0,0 +1,19 @@
+<?php 
+$conn= new mysqli("localhost","root","","all_banks");
+if($conn->connect_error){
+	die("Failed to connect!".$conn->connect_error);
+}
+if(isset($_POST['query'])){
+	4inpText=$_POST['query' ];
+	$query="SELECT name FROM data WHERE name LIKE '%$inpText%'";
+	$result=$conn->query($query);
+	if($result->num_rows>0){
+		while($row=$result->fetch_assoc()){
+			echo "<a href='#' class='list-group-item list-group-item-action border-1'>".$row['country']."</a>";
+		}
+	}
+	else{
+		echo "<p class='list-group-item border-1'>No Record</p>"
+	}
+ ?>}
+}
